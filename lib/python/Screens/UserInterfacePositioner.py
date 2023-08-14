c095a3eca01d01ce9f8e07cf13f7b160713c867b
atefganm <atef_anim_1974@yahoo.com>
2023-08-14 21:03:07 +0300

    fix crash UserInterfacePositioner then apply not exist Thanks to @atvcaptain

--- lib/python/Screens/UserInterfacePositioner.py
+++ lib/python/Screens/UserInterfacePositioner.py
@@ -21,14 +21,15 @@ def getFilePath(setting):
 	else:
 		return "/proc/stb/fb/dst_%s" % (setting)
 
-# ensure test on SystemInfo["CanChangeOsdPosition"] before calling this
+
 def setPositionParameter(parameter, configElement):
 	f = open(getFilePath(parameter), "w")
 	f.write('%X' % configElement.value)
 	f.close()
-	f = open(getFilePath("apply"), "w")
-	f.write('1')
-	f.close()
+	if fileExists(getFilePath("apply")):
+		f = open(getFilePath("apply"), "w")
+		f.write('1')
+		f.close()
 
 
 def setPositionParameter(parameter, configElement):
