a104235b9f409b516e30431a609b38ac3b66133f
your-name <atef_anim_1974@yahoo.com>
2023-11-27 13:42:12 +0300

    Revert [Updated] stream relay handling. Removed double channel allocation
    This reverts commit 412ccba.
    Thanks to @Huevos

--- lib/dvb/dvb.h
+++ lib/dvb/dvb.h
@@ -172,7 +172,6 @@ class eDVBResourceManager: public iObject, public sigc::trackable
 	void addAdapter(iDVBAdapter *adapter, bool front = false);
 	void setUsbTuner();
 
-public:
 	struct active_channel
 	{
 		eDVBChannelID m_channel_id;
@@ -182,7 +181,6 @@ public:
 		active_channel(const eDVBChannelID &chid, eDVBChannel *ch) : m_channel_id(chid), m_channel(ch) { }
 	};
 
-private:
 	std::list<active_channel> m_active_channels, m_active_simulate_channels;
 
 	ePtr<iDVBChannelList> m_list;
@@ -210,7 +208,6 @@ public:
 
 	RESULT setChannelList(iDVBChannelList *list);
 	RESULT getChannelList(ePtr<iDVBChannelList> &list);
-	RESULT getActiveChannels(std::list<active_channel> &list);
 
 	enum {
 			/* errNoFrontend = -1 replaced by more spcific messages */
