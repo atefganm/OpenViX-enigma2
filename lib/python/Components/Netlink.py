7742f256f024eb101844e6158100d9d9084e7b9a
atefganm <atef_anim_1974@yahoo.com>
2024-06-15 21:06:31 +0300

    [Netlink] remove six Thanks to @TwolDE2

--- lib/python/Components/Netlink.py
+++ lib/python/Components/Netlink.py
@@ -2,18 +2,19 @@
 # similar things, like network connections being (un)plugged.
 from os import getpid
 import socket
-import six
 
 
 class NetlinkSocket(socket.socket):
 	def __init__(self):
-		NETLINK_KOBJECT_UEVENT = 15 # hasn't landed in socket yet, see linux/netlink.h
+		NETLINK_KOBJECT_UEVENT = 15  # hasn't landed in socket yet, see linux/netlink.h
 		socket.socket.__init__(self, socket.AF_NETLINK, socket.SOCK_DGRAM, NETLINK_KOBJECT_UEVENT)
 		self.bind((getpid(), -1))
 
 	def parse(self):
-		data = six.ensure_str(self.recv(512), encoding="ascii", errors='ignore')
-		data = [x for x in data.split('\x00') if x] + [""] # avoid empty strings in the output except the final one
+		data = self.recv(512)
+		if isinstance(data, bytes):
+			data = data.decode()
+		data = [x for x in data.split('\x00') if x] + [""]  # avoid empty strings in the output except the final one
 		event = {}
 		for item in data:
 			if not item:
@@ -31,7 +32,7 @@ class NetlinkSocket(socket.socket):
 # Quick unit test (you can run this on any Linux machine)
 if __name__ == '__main__':
 	nls = NetlinkSocket()
-	print("socket no:", nls.fileno())
+	# print("socket no:", nls.fileno())
 	while 1:
 		for item in nls.parse():
 			print(repr(item))
