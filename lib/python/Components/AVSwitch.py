1e7836ee4f8aed7552c64b0ef3a3f15c72032b18
atefganm <atef_anim_1974@yahoo.com>
2025-04-22 23:26:42 +0300

    PEP8 double aggressive E225 ~ E228 and E231 Thanks to @openvix-bot

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -480,7 +480,7 @@ class AVSwitchBase:
 
 def InitAVSwitch():
 	if MACHINEBUILD == "vuduo":
-		delay_choices = [(i, ngettext("%d milisecond", "%d miliseconds", i) % i) for i in list(range(0,3000,100))]  # noqa: F821
+		delay_choices = [(i, ngettext("%d milisecond", "%d miliseconds", i) % i) for i in list(range(0, 3000, 100))]  # noqa: F821
 		config.av.passthrough_fix_long = ConfigSelection(choices=delay_choices, default=1200)
 		config.av.passthrough_fix_short = ConfigSelection(choices=delay_choices, default=100)
 		config.av.yuvenabled = ConfigBoolean(default=False)
