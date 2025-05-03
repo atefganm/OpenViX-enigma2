08eb591503e7eb3c779ce9421fe050d22f4168a5
atefganm <atef_anim_1974@yahoo.com>
2025-05-03 16:29:22 +0300

    [Fixed] Some typos [Remove] Duo4K from passthrough fix Thanks to @DimitarCC

--- lib/python/Screens/VideoMode.py
+++ lib/python/Screens/VideoMode.py
@@ -253,7 +253,7 @@ class VideoSetup(Screen, ConfigListScreen):
 		if level >= 1:
 			if BoxInfo.getItem("CanDownmixAC3"):
 				self.list.append(getConfigListEntry(_("AC3 downmix"), config.av.downmix_ac3, _("Choose whether multi channel ac3 sound tracks should be downmixed to stereo.")))
-			if SystemInfo["Passthrough_fix_needed"] and config.av.downmix_ac3.value == "passthrough":
+				if SystemInfo["Vu_EAC3_fix"] and config.av.downmix_ac3.value == "passthrough":
 				self.list.append(getConfigListEntry(_("Passthrough audio handling delay AC3"), config.av.passthrough_fix_short, _("Used to specify delay when switching between services and AC3 passthrough is enabled.")))
 				self.list.append(getConfigListEntry(_("Passthrough audio handling delay AC3+"), config.av.passthrough_fix_long, _("Used to specify delay when switching between services and AC3+/Atmos passthrough is enabled.")))
 			if BoxInfo.getItem("CanDownmixDTS"):
