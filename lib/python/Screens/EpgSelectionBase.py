6b5b2fbd7c0f4e0db07d1f7d18a9d9cb5f8598d9
atefganm <atef_anim_1974@yahoo.com>
2025-05-04 01:32:30 +0300

    PEP8 double aggressive E301 ~ E306 Thanks to @openvix-bot

--- lib/python/Screens/EpgSelectionBase.py
+++ lib/python/Screens/EpgSelectionBase.py
@@ -457,6 +457,7 @@ class EPGSelectionBase(Screen, HelpableScreen):
 			del self.eventviewDialog
 			self.eventviewDialog = None
 
+
 class EPGServiceZap:
 	def __init__(self, zapFunc):
 		self.__originalPlayingService = self.session.nav.getCurrentlyPlayingServiceOrGroup() or eServiceReference()
