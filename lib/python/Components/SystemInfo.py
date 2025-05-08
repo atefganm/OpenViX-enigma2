c202bf472da2384583211a8ac48a9cbda7805e27
atefganm <atef_anim_1974@yahoo.com>
2025-05-08 17:13:28 +0300

    PEP8 double aggressive E301 ~ E306 Thanks to @TwolDE2

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -207,6 +207,7 @@ BoxInfo.setItem("DebugLevel", eGetEnigmaDebugLvl())
 BoxInfo.setItem("InDebugMode", eGetEnigmaDebugLvl() >= 4)
 BoxInfo.setItem("ModuleLayout", getModuleLayout(), immutable=True)
 
+
 SystemInfo["CanKexecVu"] = SystemInfo["boxtype"] in ("vusolo4k", "vuduo4k", "vuduo4kse", "vuultimo4k", "vuuno4k", "vuuno4kse", "vuzero4k") and not SystemInfo["HasKexecMultiboot"]
 SystemInfo["HasUsbhdd"] = {}
 SystemInfo["ArchIsARM"] = ARCHITECTURE.startswith(("arm", "cortex"))
