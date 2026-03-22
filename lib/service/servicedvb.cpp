1bae1dee680bbbb4e9c498e9d1ddba49cb49b137
atefganm <atef_anim_1974@yahoo.com>
2026-03-22 03:29:00 +0300

    Rel7 patch cleanup
    Co-Authored-By: TwolDE <tonywhalley@t-online.de>

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -1466,10 +1466,7 @@ RESULT eDVBServicePlay::start()
 			scrambled = meta.m_scrambled;
 		}
 		else
-		{
-			/* when there is no meta file we need to handle ts/m2ts as descrambled */
-			scrambled = false;
-		}
+			scrambled = false; // Set scrambled to false if meta not exists
 		m_cue = new eCueSheet();
 		type = eDVBServicePMTHandler::playback;
 	}
@@ -1732,13 +1729,6 @@ RESULT eDVBServicePlay::setFastForward_internal(int ratio, bool final_seek)
 	}
 	else
 		return -1;
-
-	if (ffratio == 0)
-		; /* return m_decoder->play(); is done in caller*/
-	else if (ffratio != 1)
-		ret = m_decoder->setFastForward(ffratio);
-	else
-		ret = m_decoder->setTrickmode();
 
 	if (pos)
 	{
@@ -2240,7 +2230,7 @@ int eDVBServicePlay::getInfo(int w)
 		// use origiginal namespace
 		if (m_reference.isStreamRelay){
 			eServiceReferenceDVB m_parsed_ref = eServiceReferenceDVB(m_reference.compareSref);
-			if (m_parsed_ref.valid()) 
+			if (m_parsed_ref.valid())
 			{
 				return ((const eServiceReferenceDVB&)m_parsed_ref).getDVBNamespace().get();
 			}
@@ -2868,7 +2858,7 @@ RESULT eDVBServicePlay::startTimeshift()
 	if (m_timeshift_enabled)
 		return -1;
 
-		/* start recording with the data demux. */
+	/* start recording with the data demux. */
 	if (m_service_handler.getDataDemux(demux))
 		return -2;
 
@@ -2878,14 +2868,9 @@ RESULT eDVBServicePlay::startTimeshift()
 		return -3;
 
 	std::string tspath = eConfigManager::getConfigValue("config.usage.timeshift_path");
-	if (tspath == "")
+	if (tspath == "" || tspath.empty())
 	{
-		eDebug("[eDVBServicePlay] could not query timeshift path");
-		return -5;
-	}
-	if (tspath.empty())
-	{
-		eDebug("[eDVBServicePlay] timeshift path is empty");
+		eDebug("[eDVBServicePlay] could not query time shift path");
 		return -5;
 	}
 	if (tspath[tspath.length()-1] != '/')
@@ -3140,11 +3125,8 @@ void eDVBServicePlay::setCutList(ePyObject list)
 	if (!PyList_Check(list))
 		return;
 	int size = PyList_Size(list);
-	int i;
-
 	m_cue_entries.clear();
-
-	for (i=0; i<size; ++i)
+	for (int i=0; i<size; ++i)
 	{
 		ePyObject tuple = PyList_GET_ITEM(list, i);
 		if (!PyTuple_Check(tuple))
