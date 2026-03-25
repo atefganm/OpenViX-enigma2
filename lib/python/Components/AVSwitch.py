90d1fbfdd8e700680e364c7bea29498e1bda4254
atefganm <atef_anim_1974@yahoo.com>
2026-03-25 20:20:36 +0300

    Refactor passthrough audio handling and update related configurations
    Co-Authored-By: DimitarCC <dimitar_tsenev@outlook.com>

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -481,7 +481,8 @@ class AVSwitchBase:
 
 
 def InitAVSwitch():
-	config.av.passthrough_fix = ConfigBoolean(default=False)
+	if SystemInfo["Vu_EAC3_fix"] and config.av.downmix_ac3.value == "passthrough":
+		config.av.passthrough_fix = ConfigBoolean(default=True)
 	config.av.yuvenabled = ConfigBoolean(default=True)
 	colorformat_choices = {
 		"cvbs": _("CVBS"),
