5123ce210b243b5e80960ff0eeee7dd3618c0553
atefganm <atef_anim_1974@yahoo.com>
2026-03-25 07:36:05 +0300

    Revert '[enigma2] Fix SWIG interface warnings across Python bindings' This reverts commit 23bc792.
    Co-Authored-By: TwolDE <tonywhalley@t-online.de>

--- lib/dvb/dvb.h
+++ lib/dvb/dvb.h
@@ -171,7 +171,6 @@ class eDVBResourceManager: public iObject, public sigc::trackable
 	void addAdapter(iDVBAdapter *adapter, bool front = false);
 	void setUsbTuner();
 
-#ifndef SWIG
 	struct active_channel
 	{
 		eDVBChannelID m_channel_id;
@@ -180,7 +179,6 @@ class eDVBResourceManager: public iObject, public sigc::trackable
 
 		active_channel(const eDVBChannelID &chid, eDVBChannel *ch) : m_channel_id(chid), m_channel(ch) { }
 	};
-#endif
 
 	std::list<active_channel> m_active_channels, m_active_simulate_channels;
 
