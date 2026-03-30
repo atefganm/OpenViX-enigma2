421c54341313b10b2c4772f72c3989486bb4e441
atefganm <atef_anim_1974@yahoo.com>
2026-03-30 16:34:41 +0300

    Updated

--- lib/python/Screens/Wizard.py
+++ lib/python/Screens/Wizard.py
@@ -461,7 +461,7 @@ class Wizard(Screen):
 		return False
 
 	def getTranslation(self, text):
-		return _(text).replace("%s %s", "%s %s" % (SystemInfo["MachineBrand"], SystemInfo["MachineName"]))
+		return _(text).replace("%s %s", "%s %s" % (DISPLAYBRAND, MACHINENAME))
 
 	def updateText(self, firstset=False):
 		text = self.getTranslation(self.wizard[self.currStep]["text"])
