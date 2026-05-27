1f3690f25c302a6fc7f654e6d7c56dd7eb4c3aab
atefganm <atef_anim_1974@yahoo.com>
2026-05-27 18:57:14 +0300

    Updated

--- lib/python/Components/Harddisk.py
+++ lib/python/Components/Harddisk.py
@@ -222,14 +222,14 @@ class Harddisk:
 		return busName
 
 	def port(self):
-		# print(f"[Harddisk][port] physicalDevice:{self.phys_path}")
-		# print(f"[Harddisk][port] list(getDeviceDB().items() {list(getDeviceDB().items())}")
+		print(f"[Harddisk][port] physicalDevice:{self.phys_path}")
+		print(f"[Harddisk][port] list(getDeviceDB().items() {list(getDeviceDB().items())}")
 		portDescription = ""
 		for physdevprefix, pdescription in list(getDeviceDB().items()):
-			# print(f"[Harddisk][port] physdevprefix:{physdevprefix} pdescription:{pdescription}")
+			print(f"[Harddisk][port] physdevprefix:{physdevprefix} pdescription:{pdescription}")
 			if self.phys_path.replace("/sys", "").startswith(physdevprefix):
 				portDescription = _(pdescription)
-		# print(f"[Harddisk][port] portDescription:{portDescription}")
+		print(f"[Harddisk][bus] portDescription:{portDescription}")
 		return portDescription
 
 	def diskSize(self):
@@ -861,7 +861,7 @@ class HarddiskManager:
 					continue
 				mountPoint = device.get("MOUNT")
 				if mountPoint:
-					commands.append(f"/bin/umount -lf {DEVNAME.replace("/dev/", "/media/")}")
+					commands.append(f"/bin/umount -lf {DEVNAME.replace(" / dev / ", " / media / ")}")
 					ID_FS_TYPE = "auto"  # eventData.get("ID_FS_TYPE")
 					knownDevices.append(f"{ID_FS_UUID}:{mountPoint}")
 					newFstab.append(f"UUID={ID_FS_UUID} {mountPoint} {ID_FS_TYPE} defaults 0 0")
