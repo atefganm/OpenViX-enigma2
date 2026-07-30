b92e07c152efa9a497d7deab05b506b8448c7a1f
atefganm <atef_anim_1974@yahoo.com>
2026-07-30 22:51:00 +0300

    Revert 'Font - use \c to restore the original color' This reverts commit 25230f7.
    Co-Authored-By: Huevos <rowland@moralet.com>

--- lib/gdi/font.cpp
+++ lib/gdi/font.cpp
@@ -774,7 +774,6 @@ int eTextPara::renderString(const char *string, int rflags, int border, int mark
 
 	unsigned long newcolor = 0;
 	bool activate_newcolor = false;
-	bool activate_colorreset = false;
 	int nextflags = 0;
 	int pos = 0;
 	int markedlen = 0;
@@ -819,20 +818,13 @@ int eTextPara::renderString(const char *string, int rflags, int border, int mark
 							{
 								if ((i + 2 + codeidx) == uc_visual.end()) break;
 								color[codeidx] = (char)((*(i + 2 + codeidx)) & 0xff);
-								if (!isxdigit((unsigned char)color[codeidx]))
-									break;
 							}
-							isprintable = 0;
 							if (codeidx == 8)
 							{
 								newcolor = gRGB(color).argb();
 								activate_newcolor = true;
+								isprintable = 0;
 								i += 1 + codeidx;
-							}
-							else
-							{
-								activate_colorreset = true;
-								i++;
 							}
 							break;
 						}
@@ -890,8 +882,6 @@ nprint:				isprintable=0;
 					++markedpos;
 				}
 			}
-			if (activate_colorreset)
-				flags |= GS_COLORRESET;
 
 			FT_UInt index = 0;
 
@@ -925,7 +915,6 @@ nprint:				isprintable=0;
 			{
 				nextflags = 0;
 				activate_newcolor = false;
-				activate_colorreset = false;
 			}
 		} else if (nextflags&GS_ISFIRST && !glyphs.empty())
 		{
@@ -1006,15 +995,10 @@ void eTextPara::blit(gDC &dc, const ePoint &offset, const gRGB &cbackground, con
 			line_offs = *(line_offs_it++);
 			line_chars = *(line_chars_it++);
 		}
-		/* don't do colorchanges in borders */
-		if (!border)
+		if (i->flags & GS_COLORCHANGE)
 		{
-			if (i->flags & GS_COLORRESET)
-			{
-				currentforeground = foreground;
-				setcolor = true;
-			}
-			else if (i->flags & GS_COLORCHANGE)
+			/* don't do colorchanges in borders */
+			if (!border)
 			{
 				currentforeground = i->newcolor;
 				setcolor = true;
