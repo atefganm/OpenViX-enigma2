c347ea77c523a57f10adb5e4685bb170483d9559
atefganm <atef_anim_1974@yahoo.com>
2026-09-24 11:13:02 +0000

    PEP8 double aggressive E22, E224, E241, E242 and E27

--- lib/python/Plugins/SystemPlugins/Hotplug/plugin.py
+++ lib/python/Plugins/SystemPlugins/Hotplug/plugin.py
@@ -200,7 +200,7 @@ class HotPlugManager:
 							newFstab.append(f"UUID={ID_FS_UUID} {mountPoint} {ID_FS_TYPE} defaults 0 0")
 							fileWriteLines("/etc/fstab", newFstab)
 							self.callMount = True
-<						elif answer == 4:  # Permanently mount as /media/hdd
+< elif answer == 4:  # Permanently mount as /media/hdd
 							knownDevice = mountPointHdd
 							newFstab = [x for x in fstab if f"UUID={ID_FS_UUID}" not in x]
 							newFstab.append(f"UUID={ID_FS_UUID} {mountPointHdd} {ID_FS_TYPE} defaults 0 0")
